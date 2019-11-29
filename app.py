2faba987b188a5e71349cff208842b66510031c5
Hrishi <hrishikeshmane87933@gmail.com>
2019-11-29 13:47:49 +0530

    app changes

--- app.py
+++ app.py
@@ -134,6 +134,8 @@ def upload():
     return None
 
 
-if __name__ == "__main__":
-    #app.debug = True
-    app.run()
+
+port = int(os.environ.get('PORT', 5000))
+app.run(host="0.0.0.0", port=port, debug=False)
+#if __name__ == '__main__':
+#    app.run()
